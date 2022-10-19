f3cd4520d1923bd8ce486f86c3a4f6e03b6587b7
Jay Reddy <106286278+jayreddy040-510@users.noreply.github.com>
2022-10-18 22:20:56 -0700

    Time: 141 ms (60.75%), Space: 14.3 MB (28.94%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 class Solution:
-    def lengthOfLongestSubstringKDistinct(self, str1: str, k: int) -> int:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
 #         length = len(s)
 #         if length == 0 or k == 0: return 0
         
@@ -21,39 +21,39 @@ class Solution:
             
 #         return max_length
 
-#         ret = float('-inf')
-#         dict = defaultdict(int)
-#         start = 0
-#         for i in range(len(s)):
-#             dict[s[i]] += 1
-#             while len(dict) > k:
-#                 dict[s[start]] -= 1
-#                 if dict[s[start]] == 0:
-#                     del dict[s[start]]
-#                 ret = max(ret, i - start )
-#                 start += 1
+        ret = 0
+        dict = defaultdict(int)
+        start = 0
+        for i in range(len(s)):
+            dict[s[i]] += 1
+            while len(dict) > k:
+                dict[s[start]] -= 1
+                if dict[s[start]] == 0:
+                    del dict[s[start]]
+                start += 1
+            ret = max(ret, i - start + 1 )
         
-#         return ret
+        return ret
 
-        window_start = 0
-        max_length = 0
-        char_frequency = {}
+#         window_start = 0
+#         max_length = 0
+#         char_frequency = {}
 
-        # in the following loop we'll try to extend the range [window_start, window_end]
-        for window_end in range(len(str1)):
-            right_char = str1[window_end]
-            if right_char not in char_frequency:
-                char_frequency[right_char] = 0
-            char_frequency[right_char] += 1
+#         # in the following loop we'll try to extend the range [window_start, window_end]
+#         for window_end in range(len(str1)):
+#             right_char = str1[window_end]
+#             if right_char not in char_frequency:
+#                 char_frequency[right_char] = 0
+#             char_frequency[right_char] += 1
 
-            # shrink the sliding window, until we are left with 'k' distinct characters in
-            # the char_frequency
-            while len(char_frequency) > k:
-                left_char = str1[window_start]
-                char_frequency[left_char] -= 1
-                if char_frequency[left_char] == 0:
-                    del char_frequency[left_char]
-                window_start += 1  # shrink the window
-            # remember the maximum length so far
-            max_length = max(max_length, window_end-window_start + 1)
-        return max_length
+#             # shrink the sliding window, until we are left with 'k' distinct characters in
+#             # the char_frequency
+#             while len(char_frequency) > k:
+#                 left_char = str1[window_start]
+#                 char_frequency[left_char] -= 1
+#                 if char_frequency[left_char] == 0:
+#                     del char_frequency[left_char]
+#                 window_start += 1  # shrink the window
+#             # remember the maximum length so far
+#             max_length = max(max_length, window_end-window_start + 1)
+#         return max_length
